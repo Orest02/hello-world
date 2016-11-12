a160a943f6d70f2cab5de90f2020cd02f47d1290
Orest02 <orest02061996@ukr.net>
2016-11-12 18:43:01 +0100

    the drawn contours are now reder

--- circle.py
+++ circle.py
@@ -9,7 +9,7 @@ thresh=cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_
 
 im2,contours,hierarchy=cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
 
-cv2.drawContours(picta,contours, -1, (0,255,0), 3)
+cv2.drawContours(picta,contours, -1, (0,255,250), 3)
 
 cv2.imshow("test",picta)
 cv2.waitKey(0)&0xFF
